07cce964da7d924c3fbde98e330e75965cd04e63
tonipone <terseo62@gmail.com>
2021-10-13 12:07:55 +0200

    Nodemon and Morgan

--- api/controllers/contacts-controller.js
+++ api/controllers/contacts-controller.js
@@ -8,7 +8,9 @@ class ContactsController {
                 data: [
                     {fname: "Toni", lname: "Russo", phone: "333-777-888"},
                     {fname: "Roby", lname: "Esposito", phone: "444-888-999"},
-                    {fname: "Nicolas", lname: "Bianchi", phone: "555-666-222"}
+                    {fname: "Nicolas", lname: "Bianchi", phone: "555-666-222"},
+                    {fname: "New User", lname: "Contact", phone: "111-999-222"},
+                    {fname: "New User-2", lname: "Contact-2", phone: "111-999-222"}
                 ]
             })
         }
--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+//const router = express.Router();
 const contactRoutes = require('./api/routes/contacts');
 const todoRoutes = require('./api/routes/todos');
 const userRoutes = require('./api/routes/users');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 //Routes
 app.use('/contacts',contactRoutes);
 app.use('/todos',todoRoutes);
 app.use('/users',userRoutes);
 
-//app.use(router);
+//Error Handling
+app.use((req,res,next)=>{
+    const error = new Error("Route not found.");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
