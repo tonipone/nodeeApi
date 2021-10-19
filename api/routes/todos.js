0405023d56f90f7ef67c1e8a258e156ed82ce5a9
tonipone <terseo62@gmail.com>
2021-10-19 09:32:54 +0200

    Auth Todos

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const TodosController = require('../controllers/todos-controller');
+const userAuth = require('../middleware/check-auth');
 
-router.get('/', TodosController.getAll());
-router.post('/', TodosController.create());
-router.get('/:id', TodosController.findById());
-router.patch('/:id', TodosController.update());
-router.delete('/:id', TodosController.delete());
+router.get('/',userAuth, TodosController.getAll());
+router.post('/',userAuth, TodosController.create());
+router.get('/:id',userAuth, TodosController.findById());
+router.patch('/:id', userAuth,TodosController.update());
+router.delete('/:id',userAuth, TodosController.delete());
 
 module.exports = router;
